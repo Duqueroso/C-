af859f31b436153b81241e7f8a18573163c84fbe
Duqueroso <114004347+Duqueroso@users.noreply.github.com>
2024-08-09 14:30:57 -0500

    Add files via upload

---
+++ Datos.cpp
@@ -0,0 +1,23 @@
+//Tipos de datos basicos en C++
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int entero = 15;
+    cout<<entero<<endl;
+
+    float flotante = 15.5;
+    cout<<flotante<<endl;
+
+    double mayor = 3.1416;
+    cout<<mayor<<endl;
+
+    char caracter = 'a';
+    cout<<caracter<<endl;
+
+
+    return 0;
+}
